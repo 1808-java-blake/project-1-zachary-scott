cfd0bb66722eee8031264a8e50ca317bc0c15faf
Zachary Scott <zachary.douglas.scott@gmail.com>
2018-09-02 12:42:53 -0400

    front end stuff

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class Navbar extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>navbar</h1>
+      </div>
+    );
+  }
+}
